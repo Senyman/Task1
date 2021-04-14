e5b8d0b37427e46009416a9dd4a0a3a50fc07329
SenymenChic <senymenchic@mail.ru>
2021-04-14 19:34:57 +0200

    remuving connecting

--- maincode.cpp
+++ maincode.cpp
@@ -7,32 +7,34 @@ MainCode::MainCode(QObject *parent) : QObject(parent)
 
 void MainCode::logIn(QString login, QString password)
 {
+    {
         int count = 0;
         QSqlDatabase workersDB = QSqlDatabase::addDatabase("QSQLITE");  // Создаем объект для работы с базой данных
         workersDB.setDatabaseName("C://Users//User//Desktop//WorkersDB//Workers.db");   // Указываю путь к базе данных
         if(!workersDB.open()) qDebug() << "Failed to open database";
         else qDebug() << "DataBase is connected";
+        QSqlQuery qry;            // Объект для запроса информации из БД
 
-    QSqlQuery qry;            // Объект для запроса информации из БД
-    if(qry.exec("SELECT * FROM WorkersTable WHERE Login = '"+login+"' and Password = '"+password+"' "))
-    {
-        while(qry.next()) count++;
-        if(count==1) {
+        if(qry.exec("SELECT * FROM WorkersTable WHERE Login = '"+login+"' and Password = '"+password+"' ")) {
+                while(qry.next()) count++;
+                if(count==1) {
 
 #ifdef DEBUG
-            qDebug() << "Your login and password are correct ";
-            qDebug() << login;  // Отладочная
-            qDebug() << password;
-            emit sendErrorMessage("Работает!!!");
+                qDebug() << "Your login and password are correct ";
+                qDebug() << login;  // Отладочная
+                qDebug() << password;
+                emit sendErrorMessage("Работает!!!");
 #endif
+                }
+                if(count<1)  {
+                    qDebug() <<"Your login or password isn't correct";
+                    emit sendErrorMessage("Неправильно введен логин или пароль");
+                }
         }
-        if(count<1)  {
-            qDebug() <<"Your login or password isn't correct";
-            emit sendErrorMessage("Неправильно введен логин или пароль");
+        else {
+            qDebug() << "Your login or password isn't correct ";
         }
+        workersDB.close();
     }
-
-    else {
-        qDebug() << "Your login or password isn't correct ";
-    }
+    QSqlDatabase::removeDatabase("qt_sql_default_connection");
 }
